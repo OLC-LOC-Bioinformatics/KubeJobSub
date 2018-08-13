631d97ecefc5fa1f2aed12bce4fc6b0563dfb8dc
Andrew Low <andrew.low@canada.ca>
2018-08-13 11:07:11 -0400

    Azure Batch now actually downloads output files as necessary. Almost to fairly functional prototype stage

--- KubeJobSub/AzureBatch.py
+++ KubeJobSub/AzureBatch.py
@@ -3,6 +3,8 @@
 # Core python library
 import argparse
 import datetime
+import logging
+import time
 import glob
 import os
 
@@ -14,16 +16,10 @@ import azure.batch.batch_auth as batch_auth
 import azure.batch.models as batchmodels
 
 
-def upload(blob_service, item_to_upload):
-    # TODO: Make a recursive blob upload work.
-    if os.path.isfile(item_to_upload):
-        blob_service.create_blob_from_path(container_name=args.name,
-                                           blob_name=os.path.split(item_to_upload)[-1],
-                                           file_path=item_to_upload)
+# TODO: Allow for multiple tasks to be submitted in one job.
 
 
 def download_container(blob_service, container_name, output_dir):
-    # TODO: Test that this works on multi-nested stuff.
     # Modified from https://blogs.msdn.microsoft.com/brijrajsingh/2017/05/27/downloading-a-azure-blob-storage-container-python/
     generator = blob_service.list_blobs(container_name)
     for blob in generator:
@@ -43,6 +39,11 @@ def download_container(blob_service, container_name, output_dir):
 
 
 class AzureBatch:
+    """
+    Class for doing azure batch operations to make job submission relatively easy.
+    The parse_configuration_file function needs to be run to create one of these objects and set
+    attributes, or things really won't work very well (or at all).
+    """
     def __init__(self):
         # Initially, have all these attrs set to None, and set them as needed.
         self.batch_account_name = None
@@ -53,20 +54,35 @@ class AzureBatch:
         self.job_name = None
         self.command = None
         self.vm_image = None
-        self.vm_size = 'Standard_D16s_v3'  # This should be sufficient for essentially anything.
+        self.vm_size = 'Standard_D16s_v3'  # This should be sufficient for essentially anything. User can customize
+        # if they really need something bigger.
         # Both input and output will be nested lists.
         # Explain this to your future self better soon.
         self.input = list()
         self.output = list()
-        self.input_destination_dict = dict()
 
     def _login_to_batch(self):
+        """
+        Uses credentials stored in object to login to Azure batch.
+        :return: an instance of batch_client (azure.batch.batch_service_client)
+        """
         credentials = batch_auth.SharedKeyCredentials(self.batch_account_name, self.batch_account_key)
         batch_client = batch.BatchServiceClient(credentials, base_url=self.batch_account_url)
         return batch_client
 
     @staticmethod
     def _create_resource_file(blob_service, file_to_upload, input_container_name, destination_dir=False):
+        """
+        Given a file on a local machine, creates a resource file that Azure Batch can work with so that the input
+        files will be uploaded to Batch service
+        :param blob_service: Instatiated block_blob_service object (azure.storage.blob.BlockBlobService)
+        :param file_to_upload: Path to file to upload on local machine.
+        :param input_container_name: Name of the container to be used to store the input files. Must already have been
+        created.
+        :param destination_dir: Destination directory on cloud machine that process will run on. If False,
+        will be uploaded to root dir.
+        :return: An azure batchmodels resource file (azure.batch.models.ResourceFile)
+        """
         blob_name = os.path.basename(file_to_upload)
         blob_service.create_blob_from_path(container_name=input_container_name,
                                            blob_name=blob_name,
@@ -78,8 +94,6 @@ class AzureBatch:
                                              blob_name=blob_name,
                                              sas_token=sas_token)
         if destination_dir:
-            # This dictionary should be completely unnecessary now.
-            # self.input_destination_dict[os.path.split(file_to_upload)[-1]] = destination_dir
             return batchmodels.ResourceFile(file_path=os.path.join(destination_dir, os.path.split(file_to_upload)[-1]),
                                             blob_source=sas_url)
         else:
@@ -87,6 +101,10 @@ class AzureBatch:
                                             blob_source=sas_url)
 
     def upload_input_to_blob_storage(self):
+        """
+        Uploads input files to blob storage to be used with batch service.
+        :return: List of resource files (azure.batch.models.ResourceFile) to be submitted with a task
+        """
         # Instantiate our blob service! Maybe better to only do this once?
         resource_files = list()
         blob_service = BlockBlobService(account_key=self.storage_account_key,
@@ -115,10 +133,6 @@ class AzureBatch:
                                                                          file_to_upload=file_to_upload,
                                                                          input_container_name=input_container_name,
                                                                          destination_dir=destination_dir))
-                        # blob_service.create_blob_from_path(container_name=input_container_name,
-                        #                                    blob_name=os.path.split(file_to_upload)[-1],
-                        #                                    file_path=file_to_upload)
-                        # self.input_destination_dict[os.path.split(file_to_upload)[-1]] = destination_dir
         return resource_files
 
     def create_pool(self):
@@ -146,11 +160,46 @@ class AzureBatch:
         job = batch.models.JobAddParameter(self.job_name, batch.models.PoolInformation(pool_id=self.job_name))
         batch_client.job.add(job)
 
-    def prepare_output_resource_files(self):
-        # TODO
-        output_resource_files = list()
-        print(self.output)
-        return output_resource_files
+    def delete_job(self):
+        batch_client = self._login_to_batch()
+        batch_client.job.delete(job_id=self.job_name)
+
+    def delete_pool(self):
+        batch_client = self._login_to_batch()
+        batch_client.pool.delete(pool_id=self.job_name)
+
+    def prepare_output_resource_files(self, sas_url):
+        output_files = list()
+        for output_request in self.output:
+            for output_item in output_request.split():
+                output_files.append(batchmodels.OutputFile(output_item,
+                                                           destination=batchmodels.OutputFileDestination(container=batchmodels.OutputFileBlobContainerDestination(container_url=sas_url,
+                                                                                                                                                                  path=os.path.split(output_item)[0])),
+                                                           upload_options=batchmodels.OutputFileUploadOptions(
+                                                               batchmodels.OutputFileUploadCondition.task_success
+                                                           )))
+        # Also add stdout and stderr.txt log files from the azure container.
+        output_files.append(batchmodels.OutputFile('std*.txt',
+                                                   destination=batchmodels.OutputFileDestination(container=batchmodels.OutputFileBlobContainerDestination(container_url=sas_url)),
+                                                   upload_options=batchmodels.OutputFileUploadOptions(
+                                                       batchmodels.OutputFileUploadCondition.task_success
+                                                   )))
+        return output_files
+
+    def download_output_files_and_delete_container(self):
+        output_container = self.job_name.lower() + '-output'
+        blob_service = BlockBlobService(account_key=self.storage_account_key,
+                                        account_name=self.storage_account_name)
+        download_container(blob_service=blob_service,
+                           container_name=output_container,
+                           output_dir='.')  # TODO: Make this an option that user can specify.
+        blob_service.delete_container(container_name=output_container)
+
+    def delete_input_container(self):
+        input_container = self.job_name.lower() + '-input'
+        blob_service = BlockBlobService(account_key=self.storage_account_key,
+                                        account_name=self.storage_account_name)
+        blob_service.delete_container(container_name=input_container)
 
     def create_task(self, input_files):
         blob_service = BlockBlobService(account_key=self.storage_account_key,
@@ -162,27 +211,32 @@ class AzureBatch:
                                                                             permission=BlobPermissions.WRITE,
                                                                             expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=2))
         sas_url = 'https://{}.blob.core.windows.net/{}?{}'.format(self.storage_account_name, output_container_name, sas_token)
+        output_files = self.prepare_output_resource_files(sas_url)
         # TODO: this will be gone too once we actually have things set up properly.
         confindr_database = batchmodels.ResourceFile(blob_source='https://carlingst01.blob.core.windows.net/databases/confindr.tar.gz',
                                                      file_path='confindr.tar.gz')
         input_files.append(confindr_database)
         batch_client = self._login_to_batch()
-        # TODO: Change this up to actually use the user command.
-        command = "/bin/bash -c \"wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" \
-                  " && bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda && export PATH=\"$HOME/miniconda/bin:$PATH\" " \
-                  "&& conda config --add channels bioconda && conda config --add channels conda-forge && " \
-                  "conda install -c bioconda confindr && conda update bbmap && tar xf confindr.tar.gz && confindr.py -i input -o output -d databases\""
         task = batch.models.TaskAddParameter(
             id='Task1',
-            command_line=command,
+            command_line="/bin/bash -c \"{}\"".format(self.command),
             resource_files=input_files,
-            output_files=[batchmodels.OutputFile('output/confindr_report.csv',
-                          destination=batchmodels.OutputFileDestination(
-                            container=batchmodels.OutputFileBlobContainerDestination(sas_url)),
-                            upload_options=batchmodels.OutputFileUploadOptions(
-                            batchmodels.OutputFileUploadCondition.task_success))]
+            output_files=output_files
             )
         batch_client.task.add(job_id=self.job_name, task=task)
+
+    def wait_for_tasks_to_complete(self):
+        # TODO: Add an optional timeout parameter?
+        batch_client = self._login_to_batch()
+        # Check the status of all tasks associated with the job.
+        all_tasks_completed = False
+        while all_tasks_completed is False:
+            tasks = batch_client.task.list(self.job_name)
+            all_tasks_completed = True
+            for task in tasks:
+                if task.state != batchmodels.TaskState.completed:
+                    all_tasks_completed = False
+            time.sleep(30)
 
 
 def parse_configuration_file(config_file):
@@ -198,7 +252,7 @@ def parse_configuration_file(config_file):
     BATCH_ACCOUNT_URL=''
     STORAGE_ACCOUNT_NAME = ''
     STORAGE_ACCOUNT_KEY = ''
-    # Not sure if we'll actually need job name. TBD.
+    # This will be very necessary
     JOB_NAME =
     # Allow multiple input files - each one can be a unix-y mv, with the last arg being a folder to place the files
     in on cloud VM.
@@ -277,17 +331,36 @@ def check_no_attributes_none(azurebatch_object):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format='\033[92m \033[1m %(asctime)s \033[0m %(message)s ',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--configuration_file',
                         type=str,
                         required=True,
                         help='Path to your configuration file.')
+    parser.add_argument('-d', '--download_output_files',
+                        default=True,
+                        action='store_false',
+                        help='By default, output files will be downloaded from blob storage to local machine '
+                             'and the blob files deleted. Activate this to not download files and keep them in '
+                             'blob storage.')
     args = parser.parse_args()
 
+    logging.info('Reading in configuration file {}...'.format(args.configuration_file))
     azurebatch = parse_configuration_file(args.configuration_file)
     check_no_attributes_none(azurebatch)
+    logging.info('Configuration file validated. Uploading input files to blob storage...')
     resource_files = azurebatch.upload_input_to_blob_storage()
+    logging.info('Creating pool and running tasks...')
     azurebatch.create_pool()
     azurebatch.create_job()
     azurebatch.create_task(input_files=resource_files)
-    # TODO: Add wait for task to complete and then clean up pool/job/some blobs?
+    azurebatch.wait_for_tasks_to_complete()
+    logging.info('Tasks complete! Cleaning up pool...')
+    azurebatch.delete_job()
+    azurebatch.delete_pool()
+    logging.info('Downloading output files...')
+    if args.download_output_files:
+        azurebatch.download_output_files_and_delete_container()
+    azurebatch.delete_input_container()
