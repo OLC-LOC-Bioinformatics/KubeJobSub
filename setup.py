b517b6b2d16dbd9d5fee544f91d0119381b6122d
Andrew Low <andrew.low@canada.ca>
2018-08-30 13:28:30 -0400

    Update to 0.1.14 - cloud input files can now be in nested dirs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="KubeJobSub",
-    version="0.1.13",
+    version="0.1.14",
     packages=find_packages(),
     scripts=['KubeJobSub/KubeJobSub', 'KubeJobSub/AzureStorage', 'KubeJobSub/AzureBatch'],
     author="Andrew Low",
